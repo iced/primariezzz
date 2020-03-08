2c8409c9407f79eebb1ef4d23eeae038be070493
Andrew Kirilenko <andrew.kirilenko@gmail.com>
2020-03-08 20:31:51 +0300

    improvements

--- docs/js.js
+++ docs/js.js
@@ -1,5 +1,6 @@
 var clowns = undefined;
 var data = undefined;
+var total = undefined;
 var themap = undefined;
 
 function parseData(d) {
@@ -13,40 +14,72 @@ function parseData(d) {
             return e.trim();
         });
     });
+
+    total = {
+        population: 0
+    };
+    _.each(clowns, function(clown) {
+        total[clown] = 0;
+    });
     data = _.map(data, function(e) {
         r = {
             city: e[0],
             coordinates: _.map(e[1].split(" "), parseFloat),
             population: parseInt(e[2])
         };
-        var total = parseInt(e[e.length - 1]);
+        total["population"] += r["population"];
+        var t = parseInt(e[e.length - 1]);
         _.each(clowns, function(clown, i) {
             r[clown] = parseInt(e[3 + i]);
-            total += r[clown];
+            t += r[clown];
+            total[clown] += r[clown];
         });
-        r["voted"] = total;
+        r["voted"] = t;
+        r["voted_percentage"] = r["voted"] / r["population"];
         return r;
     });
+    total["voted_percentage"] = total["voted"] / total["population"];
+}
+
+function grad(palette, l, r, v) {
+    var p = (v - l) / (r - l);
+    var rgb = [Math.round(palette[0][0] + (palette[1][0] - palette[0][0]) * p),
+               Math.round(palette[0][1] + (palette[1][1] - palette[0][1]) * p),
+               Math.round(palette[0][2] + (palette[1][2] - palette[0][2]) * p)];
+    rgb = _.map(rgb, function(v) {
+        h = Number(v).toString(16);
+        if (h.length == 1) {
+            return "0" + h;
+        }
+        return h;
+    });
+    return "#" + rgb.join("");
 }
 
 function drawResults() {
-    maxp = _.max(data, function(e) {
-        return e.population;
-    }).population;
     minp = _.min(data, function(e) {
         return e.population;
     }).population;
+    maxp = _.max(data, function(e) {
+        return e.population;
+    }).population;
+    minvp = _.min(data, function(e) {
+        return e.voted_percentage;
+    }).voted_percentage;
+    maxvp = _.max(data, function(e) {
+        return e.voted_percentage;
+    }).voted_percentage;
     _.each(data, function(e) {
         tooltip = "<table>";
-        tooltip += "<tr><td><strong>" + e.city + "</strong></td><td><strong>" + e.population + "</strong></td></tr>";
+        tooltip += "<tr><td><strong>" + e.city + "</strong></td><td class=\"tooltip-right\"><strong>" + e.population + "</strong></td></tr>";
         tooltip += _.map(clowns, function(clown) {
-            return "<tr><td>" + clown + "</td><td>" + e[clown] + "</td></tr>";
+            return "<tr><td>" + clown + "</td><td class=\"tooltip-right\">" + e[clown] + "</td></tr>";
         }).join("");
         tooltip += "</table>";
         L.circle(e.coordinates, {
             radius: 1000 + (5000 - 1000) * ((e.population - minp) / (maxp - minp)),
             weight: 1,
-            color: "red",
+            color: grad([[223, 218, 135], [101, 67, 33]], minvp, maxvp, e.voted_percentage),
         }).bindTooltip(tooltip).addTo(themap);
     });
 
@@ -76,7 +109,7 @@ $(document).ready(function() {
     L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
-        id: "mapbox/streets-v8",
+        id: "mapbox/light-v9",
         tileSize: 512,
         zoomOffset: -1,
         accessToken: "pk.eyJ1IjoidGhlaWNlZCIsImEiOiJjazdqMTN2MW0wb3A1M2VvMm84enBkc21oIn0.twBqvzeUoRo1BxvwoZb87A"
